ddc753a6a2af64caf63be759aa3e605e93c07271
S.Adithya Sharma <120710617+Adithya0604@users.noreply.github.com>
2025-09-14 03:51:51 +0530

    Final Commit

--- backend/src/connection.js
+++ backend/src/connection.js
@@ -3,21 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
 const URL = process.env.MongoDB_URI;
 
 async function Connect() {
   try {
-    const conn = await mongoose.connect(URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(URL, {});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 }
-
 
 export default Connect;
