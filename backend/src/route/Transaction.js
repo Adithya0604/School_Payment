d46177762f813788ed49af7ad53b1b4c93836aa3
S.Adithya Sharma <120710617+Adithya0604@users.noreply.github.com>
2025-09-15 22:20:21 +0530

    Removed UserAuth from transaction routes

--- backend/src/route/Transaction.js
+++ backend/src/route/Transaction.js
@@ -1,12 +1,11 @@
 import express from "express";
 import { Transaction, TransactionSchool, TransactionStatus } from "../controllers/Transaction.js";
-import { UserAuth } from "../middleWares/PaymentMiddleWare.js";
 
 const router = express.Router();
 
 // Transaction
-router.get("/", UserAuth, Transaction);
-router.get("/school/:schoolId", UserAuth, TransactionSchool);
-router.get("/status/:custom_order_id", UserAuth, TransactionStatus);
+router.get("/", Transaction);
+router.get("/school/:schoolId", TransactionSchool);
+router.get("/status/:custom_order_id", TransactionStatus);
 
 export default router;
